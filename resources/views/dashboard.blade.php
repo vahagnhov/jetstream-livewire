6fccf3a26dc2b78370ffa8425b09d7362c596057
vahagnhov <vahagn505315v@gmail.com>
2023-09-23 00:54:58 +0400

    Dashboard success error messages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,22 +10,22 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
+                        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    @if(session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
+                        @if (session()->has('success'))
+                            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                                {{ session('success') }}
+                            </div>
+                        @endif
 
-                    @if(session('error'))
-                        <div class="alert alert-danger">
-                            {{ session('error') }}
-                        </div>
-                    @endif
+                        @if (session('error'))
+                            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                                {{ session('error') }}
+                            </div>
+                        @endif
 
                     {{ __('You are logged in!') }}
 
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -22,7 +22,7 @@
                         @endif
                     </div>
                     @if(session('error'))
-                        <div class="alert alert-danger">
+                            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                             {{ session('error') }}
                         </div>
                     @endif
